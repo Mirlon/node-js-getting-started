663c6b32d9dcb54a7442b389375f5178b8574f4b
Mirlon <mirlongoriya@gmail.com>
2022-06-02 20:21:24 -0700

    hello

---
+++ public/script.js
@@ -0,0 +1,14 @@
+var btn = document.getElementById("button").addEventListener('click',func);
+function func() {
+  var a = document.getElementById("number").value;
+
+  if(a >=95 && a <=100){
+    console.log(document.getElementById("A+value"));
+}
+else{
+    console.log("false");
+}
+}
+
+
+    
